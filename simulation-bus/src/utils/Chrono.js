ff2805da22ea75643360ea282bbac5111185e9fe
unknown <nilsonvargas136@gmail.com>
2023-10-22 12:55:58 -0500

    Se corrige descripción de util Chrono.js

--- simulation-bus/src/utils/Chrono.js
+++ simulation-bus/src/utils/Chrono.js
@@ -10,3 +10,22 @@ export function sleep(ms) {
 export function toMS(seconds) {
   return seconds * 1000;
 }
+
+/**
+ * Convert milliseconds to seconds
+ * @param {*} ms 
+ * @returns 
+ */
+export function toSeconds(ms) {
+  return ms / 1000;
+}
+
+/**
+ * Get delta time in milliseconds
+ * @param {number} before_time
+ * @param {number} after_time
+ * @returns {number}
+ */
+export function getDeltaTime(before_time, after_time) {
+  return after_time - before_time;
+}
